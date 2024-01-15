81eddff45909cae769b10048fa4d3e07625a775e
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-15 09:45:56 -0800

    Add files via upload

---
+++ daxxop/modules/start.py
@@ -0,0 +1,36 @@
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from daxxop import daxxop
+from config import BOT_USERNAME
+
+start_txt = """**
+land lelo mera
+**"""
+
+
+
+
+@daxxop.on_message(filters.command("start"))
+async def start(_, msg):
+    buttons = [
+        [ 
+          InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+        ],
+        [
+          InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/iam_daxx"),
+          InlineKeyboardButton("ᴅᴇᴠ", url="https://t.me/iam_daxx"),
+          InlineKeyboardButton("ʜᴇʟᴘ", callback_data="help_")
+        ]]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await msg.reply_photo(
+        photo="",
+        caption=start_txt,
+        reply_markup=reply_markup
+    )
+
+
+
+
+
