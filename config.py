499534de417920db5edbaa07ae300f31d8ab630a
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-15 23:20:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "26850449"))
 API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-BOT_USERNAME = getenv("BOT_USERNAME", "daxxopbot")
+BOT_TOKEN = getenv("BOT_TOKEN", "6827090837:AAEOntEZqys0ykQogIuQorHUFTp1JjDdM5E")
+BOT_USERNAME = getenv("BOT_USERNAME", "urXdestinyBot")
 OWNER_ID = int(getenv("OWNER_ID", "6664582540"))
