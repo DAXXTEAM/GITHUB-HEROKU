ff129fe4b80c5ff3c6e20d6a24da9aaf6fcc3d91
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-15 09:43:49 -0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+from os import getenv
+
+
+API_ID = int(getenv("API_ID", "26850449"))
+API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_USERNAME = getenv("BOT_USERNAME", "daxxopbot")
+OWNER_ID = int(getenv("OWNER_ID", "6664582540"))
