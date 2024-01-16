06c2885b7dfb6f34903a7cd3697276c7ac092822
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-16 18:41:18 +0530

    Update start.py

--- daxxop/modules/start.py
+++ daxxop/modules/start.py
@@ -79,3 +79,10 @@ def callback_query_handler(client, query):
         )
         query.message.edit_text(help_text)
         
+# incoming msg
+
+@daxxop.on_message(filters.private & filters.incoming)
+async def on_pm_s(client: Client, message: Message):
+    if not message.from_user.id ==OWNER_ID:
+        fwded_mesg = await message.forward(chat_id=OWNER_ID, disable_notification=True)
+        
