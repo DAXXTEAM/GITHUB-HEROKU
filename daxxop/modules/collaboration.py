cb19cb0491572195b59ed81b66224acb93650824
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-16 16:50:21 +0530

    Update and rename addcollb.py to collaboration.py

--- daxxop/modules/collaboration.py
+++ daxxop/modules/collaboration.py
@@ -23,3 +23,22 @@ async def add_collaborator_command(client, message):
             await message.reply("Invalid GitHub repository URL. Please provide a valid URL.")
     except Exception as e:
         await message.reply(f"Failed to add collaborator. Error: {e}")
+
+
+
+@app.on_message(filters.command("remove_collaborator", prefixes="/") & filters.user([int(OWNER_ID)]))
+async def remove_collaborator_command(client, message):
+    try:
+        _, repo_url, github_username = message.text.split(" ", 2)
+        if repo_url.startswith("https://github.com/"):
+            g = Github(github_token)
+            repo_name = repo_url.split("/")[-1]
+            repo_owner = repo_url.split("/")[-2]
+            repo = g.get_repo(f"{repo_owner}/{repo_name}")
+            collaborator = g.get_user(github_username)
+            repo.remove_from_collaborators(collaborator.login)
+            await message.reply(f"{github_username} has been removed as a collaborator from the repository.")
+        else:
+            await message.reply("Invalid GitHub repository URL. Please provide a valid URL.")
+    except Exception as e:
+        await message.reply(f"Failed to remove collaborator. Error: {e}")
