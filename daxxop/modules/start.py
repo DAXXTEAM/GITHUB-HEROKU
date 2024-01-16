0011c9995c632b367a7010b714a39dbf5df8080c
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-16 16:59:17 +0530

    Update start.py

--- daxxop/modules/start.py
+++ daxxop/modules/start.py
@@ -42,5 +42,19 @@ async def start(_, msg):
 
 
 
+#➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪➪
 
 
+@app.on_message(filters.command("help", prefixes="/"))
+def help_command(_, message):
+    help_message = (
+        "GitHub Control Bot Commands:\n"
+        "/start - Start the bot\n"
+        "/help - Display this help message\n"
+        "/allrepo - List your GitHub repositories\n"
+        "/create_repo - Create a new GitHub repository\n"
+        "/delrepo - Delete a GitHub repository\n"
+        "/add_collaborator - Add a collaborator to a GitHub repository\n"
+        "/remove_collaborator - Remove a collaborator from a GitHub repository"
+    )
+    message.reply_text(help_message)
