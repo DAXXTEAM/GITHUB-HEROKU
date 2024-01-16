887cec645908e0abe159a4e36902834f8f2b3e66
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-16 14:15:29 +0530

    Update and rename group.py to misc.py

---
+++ daxxop/modules/misc.py
@@ -0,0 +1,88 @@
+from pyrogram.types import Message
+import asyncio, os, time, aiohttp
+import aiohttp
+from pyrogram import filters
+from daxxop import daxxop as app
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+# --------------------------------------------------------------------------------- #
+
+@app.on_message(filters.command("id"))
+def get_user_chat_id(_: Client, message: Message):
+    user_id = message.from_user.id
+    chat_id = message.chat.id
+
+    response_text = (
+        f"ʏᴏᴜʀ ɪᴅ: `{user_id}`\n"
+        f"ᴄʜᴀᴛ ɪᴅ: `{chat_id}`"
+    )
+
+    message.reply_text(response_text)
+# --------------------------------------------------------------------------------- #
+
+
+@app.on_callback_query(filters.regex("^close_data"))
+async def close_callback(_, query):
+    chat_id = query.message.chat.id
+    await query.message.delete()
+
+# --------------------------------------------------------------------------------- #
+
+# --------------------------------------------------------------------------------- #
+
+
+@app.on_message(filters.command(["github", "git"]))
+async def github(_, message):
+    if len(message.command) != 2:
+        await message.reply_text("/git DAXXTEAM")
+        return
+
+    username = message.text.split(None, 1)[1]
+    URL = f'https://api.github.com/users/{username}'
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(URL) as request:
+            if request.status == 404:
+                return await message.reply_text("404")
+
+            result = await request.json()
+
+            try:
+                url = result['html_url']
+                name = result['name']
+                company = result['company']
+                bio = result['bio']
+                created_at = result['created_at']
+                avatar_url = result['avatar_url']
+                blog = result['blog']
+                location = result['location']
+                repositories = result['public_repos']
+                followers = result['followers']
+                following = result['following']
+
+                caption = f"""ɢɪᴛʜᴜʙ ɪɴғᴏ ᴏғ {name}
+                
+ᴜsᴇʀɴᴀᴍᴇ: {username}
+ʙɪᴏ: {bio}
+ʟɪɴᴋ: [Here]({url})
+ᴄᴏᴍᴩᴀɴʏ: {company}
+ᴄʀᴇᴀᴛᴇᴅ ᴏɴ: {created_at}
+ʀᴇᴩᴏsɪᴛᴏʀɪᴇs: {repositories}
+ʙʟᴏɢ: {blog}
+ʟᴏᴄᴀᴛɪᴏɴ: {location}
+ғᴏʟʟᴏᴡᴇʀs: {followers}
+ғᴏʟʟᴏᴡɪɴɢ: {following}"""
+
+            except Exception as e:
+                print(str(e))
+                pass
+# --------------------------------------------------------------------------------- #
+    
+    # Create an inline keyboard with a close button
+    close_button = InlineKeyboardButton("Close", callback_data="close")
+    inline_keyboard = InlineKeyboardMarkup([[close_button]])
+# --------------------------------------------------------------------------------- #
+    
+    # Send the message with the inline keyboard
+    await message.reply_photo(photo=avatar_url, caption=caption, reply_markup=inline_keyboard)
+# --------------------------------------------------------------------------------- #
