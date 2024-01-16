60a589590f710e7c5e1b8e2f92f567f5a0894fe4
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-16 15:58:32 +0530

    Create create_repo.py

---
+++ daxxop/modules/create_repo.py
@@ -0,0 +1,33 @@
+from pyrogram import Client, filters
+import requests
+import config
+from daxxop import daxxop as app
+
+GITHUB_TOKEN = config.GIT_TOKEN
+
+
+
+def create_github_repo(repo_name):
+    url = "https://api.github.com/user/repos"
+    headers = {"Authorization": f"token {GITHUB_TOKEN}"}
+    data = {"name": repo_name, "auto_init": True}
+
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code == 201:
+        repo_link = response.json().get("html_url")
+        return f"Rᴇᴘᴏsɪᴛᴏʀʏ ᴄʀᴇᴀᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ!\nYᴏᴜʀ ʀᴇᴘᴏ ʟɪɴᴋ: {repo_link}"
+    else:
+        return f"Failed to create repository. Error: {response.text}"
+
+
+@app.on_message(filters.command("create_repo", prefixes="/"))
+def create_repo_command(client, message):
+    command_parts = message.text.split(" ", 1)
+
+    if len(command_parts) == 2:
+        repo_name = command_parts[1].strip()
+        response_text = create_github_repo(repo_name)
+        message.reply_text(response_text)
+    else:
+        message.reply_text("Iɴᴠᴀʟɪᴅ ᴄᴏᴍᴍᴀɴᴅ ᴜsᴀɢᴇ. Pʟᴇᴀsᴇ ᴜsᴇ /create_repo <repository_name>")
