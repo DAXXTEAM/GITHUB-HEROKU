33f3ee14aa6a6c3402f91acdcb77909cf92c2879
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-16 18:54:59 +0530

    Update start.py

--- daxxop/modules/start.py
+++ daxxop/modules/start.py
@@ -1,4 +1,4 @@
-from pyrogram import filters
+from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from daxxop import daxxop as app
 from config import BOT_USERNAME
