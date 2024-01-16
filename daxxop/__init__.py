e31d7238efce0a4e4aebc25ec836563fa1619de3
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-16 17:58:18 +0530

    Update __init__.py

--- daxxop/__init__.py
+++ daxxop/__init__.py
@@ -5,16 +5,22 @@ from importlib import import_module
 from os import listdir, path
 from dotenv import load_dotenv
 from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN, LOGGER_ID
+from config import API_ID, API_HASH, BOT_TOKEN
+
+
+
 
 loop = asyncio.get_event_loop()
 load_dotenv()
 boot = time.time()
 
+
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
     level=logging.INFO,
 )
+
+
 
 daxxop = Client(
     ":daxxop:",
@@ -23,11 +29,11 @@ daxxop = Client(
     bot_token=BOT_TOKEN,
 )
 
+
+
 async def daxxop_bot():
     global BOT_ID, BOT_NAME, BOT_USERNAME
-    await daxxop.start()  # Start the client to synchronize with Telegram servers
-    await asyncio.sleep(2)  # Wait for a moment to ensure synchronization
-    await daxxop.send_message(LOGGER_ID, text=" I AM ALIGE !!")
+    await daxxop.start()
     getme = await daxxop.get_me()
     BOT_ID = getme.id
     BOT_USERNAME = getme.username
@@ -35,6 +41,6 @@ async def daxxop_bot():
         BOT_NAME = getme.first_name + " " + getme.last_name
     else:
         BOT_NAME = getme.first_name
-    await daxxop.stop()
+
 
 loop.run_until_complete(daxxop_bot())
