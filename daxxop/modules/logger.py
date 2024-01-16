269e50b45b007b04084ddddd67da64d28e43f280
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-16 18:11:07 +0530

    Create logger.py

---
+++ daxxop/modules/logger.py
@@ -0,0 +1,19 @@
+import requests
+import config
+
+def bot_start(token, chat_id, message):
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    params = {"chat_id": chat_id, "text": message}
+    
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        print("Message sent successfully!")
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to send message: {e}")
+
+telegram_bot_token = config.BOT_TOKEN
+telegram_chat_id = '@herokufreecc'
+start_message = "done"
+
+bot_start(telegram_bot_token, telegram_chat_id, start_message)
