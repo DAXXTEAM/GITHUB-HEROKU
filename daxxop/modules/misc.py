5c4d5654285bb76e220697e246c3c0dfc8ce5992
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-17 11:40:20 +0530

    Update misc.py

--- daxxop/modules/misc.py
+++ daxxop/modules/misc.py
@@ -89,37 +89,3 @@ async def github(_, message):
     # Send the message with the inline keyboard
     await message.reply_photo(photo=avatar_url, caption=caption, reply_markup=inline_keyboard)
 # --------------------------------------------------------------------------------- #
-
-
-
-WEL_GIFS = [
-    "https://telegra.ph/file/bd77135692ad077d34d64.mp4",
-    "https://telegra.ph/file/df6e25fd22b0562309640.mp4"
-]
-
-def create_close_button():
-    return InlineKeyboardMarkup(
-        [[InlineKeyboardButton("๏ ᴄʟᴏsᴇ ๏", callback_data="close_data")]]
-    )
-
-@app.on_message(filters.new_chat_members)
-async def welcome(_, m: Message):
-    random_wel_gif = random.choice(WEL_GIFS)
-    join_date = datetime.utcfromtimestamp(m.date.timestamp()).strftime('%Y-%m-%d')
-    
-    welcome_message = await m.reply_animation(
-        random_wel_gif,
-        caption=f"Hɪɪ ᴅᴇᴀʀ {m.from_user.mention}\nᴡᴇʟᴄᴏᴍᴇ ᴛᴏ {m.chat.title}!\n• Jᴏɪɴᴇᴅ ᴀᴛ: {join_date}\n┏━━━━━━━━━━━━━━━━━━━━┓\n• ꜰᴏʟʟᴏᴡ ᴏᴜʀ ʀᴜʟᴇꜱ ᴘʟᴇᴀꜱᴇ✨\n\n╚»『ᴅᴏɴ'ᴛ ᴜᴘʟᴏᴀᴅ 18+ ᴍᴀᴛᴇʀɪᴀʟ\n╚»『 ꜱᴘᴀᴍᴍɪɴɢ & ꜰɪɪɢʜᴛ ɴᴏᴛ ᴀʟʟᴏᴡᴇᴅ✨\n╚»『 ᴏᴡɴᴇʀ - ꜱᴇᴄʀᴇᴛ\n╚»『 ɢɪʀʟꜱ ᴅᴍ ɴᴏᴛ ᴀʟʟᴏᴡᴇᴅ\n╚» ᴏᴛʜᴇʀᴇ ᴡɪsᴇ ʏᴏᴜ ᴡɪʟʟ ɴᴇ ʙᴀɴᴇᴅ\n╚»『 ᴇɴᴊᴏʏ ᴏᴜʀ ɢʀᴏᴜᴘ\n┗━━━━━━━━━━━━━━━━━━━━",
-    )
-
-    # Use the message ID of the welcome message for replies in the future
-    app.set_my_commands(welcome_message.message_id)
-
-@app.on_message(filters.left_chat_member)
-async def member_has_left(_, m: Message):
-    left_gif = "https://telegra.ph/file/d28047520fad932521368.mp4"
-    await m.reply_animation(
-        animation=left_gif,
-        caption=f"Sᴀᴅ Tᴏ Sᴇᴇ Yᴏᴜ Lᴇᴀᴠɪɴɢ {m.from_user.mention}\nTᴀᴋᴇ Cᴀʀᴇ!\n",
-        reply_markup=create_close_button()
-    )
