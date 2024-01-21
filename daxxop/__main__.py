1b4c0169eebcaa41d33e18ee59fa5faa3455513b
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-21 18:46:18 +0530

    Update __main__.py
    Signed-off-by: DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>

--- daxxop/__main__.py
+++ daxxop/__main__.py
@@ -32,24 +32,24 @@ from daxxop.modules import ALL_MODULES
 
 loop = asyncio.get_event_loop()
 
-async def send_startup_message(client, logger_id):
+async def send_startup_message(Client, logger_id):
     
     startup_message = "Bot has successfully started!"
-    await client.send_message(logger_id, startup_message)
+    await Client.send_message(logger_id, startup_message)
 
-async def daxxpapa_boot(client, start_in_logger_groups=False):
+async def daxxpapa_boot(Client, start_in_logger_groups=False):
     for all_module in ALL_MODULES:
         importlib.import_module("daxxop.modules." + all_module)
 
     if start_in_logger_groups:
-        await send_startup_message(client, LOGGER_ID)
+        await send_startup_message(Client, LOGGER_ID)
 
     print("»»»» ʙᴏᴛ ᴅᴇᴘʟᴏʏ sᴜᴄᴄᴇssғᴜʟʟʏ. ✨ 🎉")
     await idle()
     print("»» ɢᴏᴏᴅ ʙʏᴇ ! sᴛᴏᴘᴘɪɴɢ ʙᴏᴛ.")
 
 if __name__ == "__main__":
-    loop.run_until_complete(client.start())
-    loop.run_until_complete(daxxpapa_boot(client, start_in_logger_groups=True))
-    loop.run_until_complete(client.stop())
+    loop.run_until_complete(Client.start())
+    loop.run_until_complete(daxxpapa_boot(Client, start_in_logger_groups=True))
+    loop.run_until_complete(Client.stop())
     
