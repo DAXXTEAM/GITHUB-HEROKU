a306053cf90eac5d0dd78c3d0ccdd2da8a79ae74
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-21 19:10:39 +0530

    Update config.py
    Signed-off-by: DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "26850449"))
 API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
-BOT_TOKEN = getenv("BOT_TOKEN", "6065719968:AAGMmJJvkYitovIxkvZXg53Qc7z3BoGFwgA")
+BOT_TOKEN = getenv("BOT_TOKEN", "6552219333:AAEwGYFh4_0QdBtccXp0D3lDBxGfqxo8nXE")
 BOT_USERNAME = getenv("BOT_USERNAME", "DEVLOPERROBOT")
 OWNER_ID = int(getenv("OWNER_ID", "6664582540"))
 GIT_TOKEN = getenv("GIT_TOKEN", "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")
