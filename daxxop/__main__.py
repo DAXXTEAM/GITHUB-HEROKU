ad27f1a8057d579ed642a987f51676b4f7898493
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-21 19:06:21 +0530

    Update __main__.py
    Signed-off-by: DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>

--- daxxop/__main__.py
+++ daxxop/__main__.py
@@ -1,11 +1,12 @@
 import asyncio
 import importlib
+import config
 from config import LOGGER_ID
 from pyrogram import idle
 from daxxop import daxxop
 from daxxop.modules import ALL_MODULES
 
-LOGGER_ID = -1001802990747
+#LOGGER_ID = -1001802990747
 
 
 loop = asyncio.get_event_loop()
@@ -16,7 +17,7 @@ async def daxxpapa_boot():
     print("»»»» ʙᴏᴛ ᴅᴇᴘʟᴏʏ sᴜᴄᴄᴇssғᴜʟʟʏ. ✨ 🎉")
     await idle()
     print("»» ɢᴏᴏᴅ ʙʏᴇ ! sᴛᴏᴘᴘɪɴɢ ʙᴏᴛ.")
-    await daxxop.send_message(LOGGER_ID, "**Bot Started !**")
+    await daxxop.send_message(config.LOGGER_ID, "**Bot Successful Started!**")
 
 if __name__ == "__main__":
     loop.run_until_complete(daxxpapa_boot())
