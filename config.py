861987ff615e335eef425074322002c87ccf40c8
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-22 11:30:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+"""
 from os import getenv
 
 
@@ -8,3 +9,17 @@ BOT_USERNAME = getenv("BOT_USERNAME", "DEVLOPERROBOT")
 OWNER_ID = int(getenv("OWNER_ID", "6664582540"))
 GIT_TOKEN = getenv("GIT_TOKEN", "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")
 LOGGER_ID = getenv("LOGGER_ID", "-1001802990747")
+
+"""
+# ------------------------------------------------
+
+import os
+from os import getenv
+
+API_ID = int(os.environ.get("YOUR_API_ID"))
+API_HASH = os.environ.get("YOUR_API_HASH")
+BOT_TOKEN = os.environ.get("YOUR_BOT_TOKEN")
+BOT_USERNAME = os.environ.get("YOUR_BOT_USERNAME")
+OWNER_ID = int(os.environ.get("YOUR_OWNER_ID"))
+GIT_TOKEN = os.environ.get("YOUR_GIT_TOKEN")
+LOGGER_ID = int(os.environ.get("YOUR_LOGGER_ID"))
