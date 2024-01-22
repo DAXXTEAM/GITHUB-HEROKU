a08b021143108e16d9fcfa38d7798b0aeecb55a6
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-22 11:45:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,4 @@
-"""
-from os import getenv
 
-
-API_ID = int(getenv("API_ID", "26850449"))
-API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
-BOT_TOKEN = getenv("BOT_TOKEN", "6065719968:AAHtBO5sKY-Y5UxT1fMquJ0ghROoy2rYibE")
-BOT_USERNAME = getenv("BOT_USERNAME", "DEVLOPERROBOT")
-OWNER_ID = int(getenv("OWNER_ID", "6664582540"))
-GIT_TOKEN = getenv("GIT_TOKEN", "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")
-LOGGER_ID = getenv("LOGGER_ID", "-1001802990747")
-
-"""
 # ------------------------------------------------
 
 import os
