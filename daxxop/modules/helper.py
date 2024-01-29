e89b8872dbe0e9337267766211a0874dd17b975d
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-29 16:36:25 +0530

    Update helper.py

--- daxxop/modules/helper.py
+++ daxxop/modules/helper.py
@@ -6,30 +6,28 @@ from daxxop import daxxop as app
 def help_handler(client, message):
     help_text = "This is a sample bot.\n\nYou can use the following commands:"
     
-    
     buttons = [
         [
-        InlineKeyboardButton("Button 1", callback_data="button1")
-        InlineKeyboardButton("Button 2", callback_data="button2")
-        InlineKeyboardButton("Button 3", callback_data="button3")
-    ],
-        [
-        InlineKeyboardButton("Button 4", callback_data="button4")
-        InlineKeyboardButton("Button 5", callback_data="button5")
-        InlineKeyboardButton("Button 6", callback_data="button6")
-
+            InlineKeyboardButton("Button 1", callback_data="button1"),
+            InlineKeyboardButton("Button 2", callback_data="button2"),
+            InlineKeyboardButton("Button 3", callback_data="button3")
         ],
-
-      [
-        InlineKeyboardButton("Button 7", callback_data="button7")
-        InlineKeyboardButton("Button 8", callback_data="button8")
-        InlineKeyboardButton("Button 9", callback_data="button9")
-      ],
         [
-        InlineKeyboardButton("Button 10", callback_data="button10")
+            InlineKeyboardButton("Button 4", callback_data="button4"),
+            InlineKeyboardButton("Button 5", callback_data="button5"),
+            InlineKeyboardButton("Button 6", callback_data="button6")
+        ],
+        [
+            InlineKeyboardButton("Button 7", callback_data="button7"),
+            InlineKeyboardButton("Button 8", callback_data="button8"),
+            InlineKeyboardButton("Button 9", callback_data="button9")
+        ],
+        [
+            InlineKeyboardButton("Button 10", callback_data="button10")
+        ]
     ]
-    ],
   
-    reply_markup = InlineKeyboardMarkup([buttons])
+    reply_markup = InlineKeyboardMarkup(buttons)
    
     message.reply_text(help_text, reply_markup=reply_markup)
+    
