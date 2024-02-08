7912263b789c8ab7485ef6654762a7d0fc23f1a0
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-08 21:56:21 +0530

    Update copyright.py

--- daxxop/modules/copyright.py
+++ daxxop/modules/copyright.py
@@ -6,14 +6,15 @@ from pyrogram.types import Message
 
 @app.on_edited_message(filters.group & ~filters.me)
 async def delete_edited_messages(client, edited_message):
+    if edited_message.from_user.is_admin:
+        return
     await edited_message.delete()
 
-
-    
 @app.on_message(filters.animation | filters.audio | filters.document | filters.photo | filters.sticker | filters.video)
 async def keep_reaction_message(client, message: Message):
     pass 
-# -------------------------------
+
+# -----------------------------------------------------
 
 async def delete_pdf_files(client, message):
     if message.document and message.document.mime_type == "application/pdf":
@@ -26,3 +27,4 @@ async def delete_pdf_files(client, message):
 @app.on_message(filters.group & filters.document)
 async def message_handler(client, message):
     await delete_pdf_files(client, message)
+
